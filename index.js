95ead9e129ec5a8a9e8964908d763342121cb75f
ELIZABETH LE <elizabethlemk@yahoo.com>
2018-11-28 01:19:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ function takeANumber(katzDeliLine, name){
 
 function nowServing(deliLine){
   var i = 0
+<<<<<<< HEAD
   if (deliLine.length > 0){
     return `Currently serving ${deliLine.shift()}.`;
+=======
+  if (i === 0){
+    return `Currently serving ${deliLine[i]}.`;
+  deliLine.unshift()
+>>>>>>> 63ee56ac5e09bd8a4f686ad634f5a78b4134eea1
 } else{
     return `There is nobody waiting to be served!`
   }
